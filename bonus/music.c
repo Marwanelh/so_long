b6213363efb0ab1ff248d8ce61073e6a77cf0f46
mael-hou <marwan.elhour56@gmail.com>
2026-01-07 19:02:20 +0700

    Make music playback fail silently - redirect errors to /dev/null

--- bonus/music.c
+++ bonus/music.c
@@ -12,6 +12,18 @@
 
 #include "../so_long_bonus.h"
 
+static void	redirect_errors(void)
+{
+	int	fd;
+
+	fd = open("/dev/null", O_WRONLY);
+	if (fd != -1)
+	{
+		dup2(fd, STDERR_FILENO);
+		close(fd);
+	}
+}
+
 void	init_music(t_game *game)
 {
 	pid_t	pid;
@@ -19,16 +31,17 @@ void	init_music(t_game *game)
 	pid = fork();
 	if (pid == 0)
 	{
+		redirect_errors();
 		execlp("paplay", "paplay", "--volume=65536", "sound/music.mp3", NULL);
 		execlp("ffplay", "ffplay", "-nodisp", "-autoexit", "-loglevel", "quiet",
 			"sound/music.mp3", NULL);
 		execlp("mpv", "mpv", "--no-video", "--quiet", "sound/music.mp3", NULL);
 		execlp("cvlc", "cvlc", "--play-and-exit", "--no-video",
 			"sound/music.mp3", NULL);
-		execlp("aplay", "aplay", "sound/music.mp3", NULL);
-		exit(1);
+		execlp("aplay", "aplay", "-q", "sound/music.mp3", NULL);
+		exit(0);
 	}
-	else
+	else if (pid > 0)
 	{
 		game->music_pid = pid;
 	}
